263b16e9178afe8aeb61dc147268a5847a808c05
Dirk-Peetermans-home <peetermans.dirk@telenet.be>
2020-09-24 09:51:41 +0200

    Commit ExA1.java

---
+++ exA1/ExA1.java
@@ -0,0 +1,14 @@
+public class ExA1 {
+	
+			
+	public static void main(String[] args) {
+		
+	 String title= "Java Reskilling";
+	 int numberOfDays = 21;
+	 double pricePerDay = 1000.5;
+	 boolean priorKnowledgeReq = true;
+		
+	 System.out.println("Course title: " + title + " \r\nCourse duration: " + numberOfDays + "\r\nPrice per day: " + pricePerDay + "\r\nPrior knowledge needed: " + priorKnowledgeReq);
+	}
+}
+
