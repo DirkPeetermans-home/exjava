8220e0f4202a9564868cb83028bf07eb588cea21
Dirk-Peetermans-home <peetermans.dirk@telenet.be>
2020-09-24 10:08:51 +0200

    Commit ExA2.java

--- exA2/ExA2.java
+++ exA2/ExA2.java
@@ -1,14 +1,22 @@
-public class ExA1 {
-	
-			
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExA2 {
+				
 	public static void main(String[] args) {
 		
 	 String title= "Java Reskilling";
 	 int numberOfDays = 21;
 	 double pricePerDay = 1000.5;
 	 boolean priorKnowledgeReq = true;
+	 List<String> instructorList = new ArrayList<String>();
+	 
+	 instructorList.add("Sandy Schillebeeckx");
+	 instructorList.add("xxxx yyyyy");
 		
 	 System.out.println("Course title: " + title + " \r\nCourse duration: " + numberOfDays + "\r\nPrice per day: " + pricePerDay + "\r\nPrior knowledge needed: " + priorKnowledgeReq);
+	System.out.println("There are " + instructorList.size() + " instructors for this course.") ;
 	}
+	
 }
 
