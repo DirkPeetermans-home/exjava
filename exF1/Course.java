152c007a8416fdae7d262f8f879de2836f854acc
Dirk-Peetermans-home <peetermans.dirk@telenet.be>
2020-09-28 12:46:42 +0200

    exF1

--- exF1/Course.java
+++ exF1/Course.java
@@ -1,86 +1,107 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
 
-public class ExD1 {
-				
-	public static void main(String[] args) {
-		
-	Scanner myInput = new Scanner(System.in);
-	double totalPrice;
-	String cheapOrExpensive;
-	int numberOfDays = 0;
-	double pricePerDay = 0;
-	boolean daysOK = false;
+public class Course {
 	
-	System.out.println("Enter course title : ");		
-	String title = myInput.nextLine();
-	 
-		
-	while (!daysOK) {
-	try {
-		System.out.println("Course duration : ");
-		numberOfDays = Integer.parseInt(myInput.nextLine());
-		daysOK = true;
-	} catch (NumberFormatException e) {
-		System.out.println("Please enter an Integer value here");	
-		}	
+	private String title;
+	private int numberOfDays;
+	private double pricePerDay;
+	private boolean priorKnowledgeRequired;	
+	private List<String> instructors = new ArrayList<String>();
+	
+
+	public Course(String title, int numberOfDays, double pricePerDay, boolean priorKnowledgeRequired) {
+		this.title = title;
+		this.numberOfDays = numberOfDays;
+		this.pricePerDay = pricePerDay;
+		this.priorKnowledgeRequired = priorKnowledgeRequired;
 	}
 	
-	daysOK = false;
-	
-	while (!daysOK) {		
-	try {
-		System.out.println("Price per day : ");
-		pricePerDay = Double.parseDouble(myInput.nextLine());
-		daysOK = true;
-	} catch (NumberFormatException e) {
-		System.out.println("Please enter an Double/Interger value here");
-			}
-	}
-		 
-	 boolean priorKnowledgeReq = true;
-	 
-	 //Read instructors from file and put them in an Arraylist
-	 
-	 List<String> instructorList = new ArrayList<String>();
-	 BufferedReader br = null;
-	 
-	 try {
-		 br = new BufferedReader(new FileReader("instructors.txt"));
-		 String lineFromFile = br.readLine();
-	 while (lineFromFile != null) {
-		 instructorList.add(lineFromFile);
-		 lineFromFile = br.readLine();
-	 }
-	 } catch (IOException ioe) {
-		 System.out.println("Problem with file :" + ioe);
-		 System.exit(0);
-	 }
-	
-	  
-	  
-		 
-	 printInfo(title, numberOfDays, pricePerDay, priorKnowledgeReq, instructorList, totalPriceCalc(numberOfDays,priorKnowledgeReq, pricePerDay));
-	 
-	
+	public String getTitle() {
+		return title;
 	}
 	
-	private static double totalPriceCalc(int numberOfDays, boolean priorKnowledgeReq, double pricePerDay) {
-		double totalPrice=numberOfDays * pricePerDay;
-		if (!(numberOfDays > 3 && priorKnowledgeReq )) {
-		 totalPrice*=1.21;
+	public void setTitle(String title) {
+		this.title=title;
+	}
+	
+	public int getNumberOfDays() {
+		return numberOfDays;
+	}
+
+	public void setNumberOfDays(int numberOfDays) {
+		this.numberOfDays = numberOfDays;
+	}
+
+	public double getPricePerDay() {
+		return pricePerDay;
+	}
+
+	public void setPricePerDay(double pricePerDay) {
+		this.pricePerDay = pricePerDay;
+	}
+
+	public boolean isPriorKnowledgeRequired() {
+		return priorKnowledgeRequired;
+	}
+
+	public void setPriorKnowledgeRequired(boolean priorKnowledgeRequired) {
+		this.priorKnowledgeRequired = priorKnowledgeRequired;
+	}
+
+	public List<String> getInstructors() {
+		return instructors;
+	}
+
+	public void setInstructors(List<String> instructors) {
+		this.instructors = instructors;
+	}
+
+	public void addInstructor(String instructor) {
+		instructors.add(instructor);
+	}
+	
+	public void removeInstructor(String instructor) {
+		instructors.remove(instructor);
+	}
+	
+	public  double calculatePrice() {
+		double totalPrice = numberOfDays * pricePerDay;
+		if (!(numberOfDays > 3 && priorKnowledgeRequired)) {
+			totalPrice *= 1.21;
 		}
 		return totalPrice;
-		
 	}
 	
-	private static void printInfo(String title, int numberOfDays, double pricePerDay, boolean priorKnowledgeReq, List instructorList, double totalPrice) {
-	System.out.println("Course title: " + title + " \r\nCourse duration: " + numberOfDays + "\r\nPrice per day: " + pricePerDay + "\r\nPrior knowledge needed: " + priorKnowledgeReq);
-	System.out.println("There are " + instructorList.size() + " instructors for this course.") ;
-	System.out.println("The total price is : " + totalPrice);
-}
+	
+	public  void printInfo() throws IOException {
+		BufferedWriter bw = Files.newBufferedWriter(Paths.get("courseinfo"+title+".txt"));
+		double totalPrice = this.calculatePrice();
+		bw.write("The " + this.title + " course takes " + this.numberOfDays + " days and costs " + totalPrice
+				+ " euros. \nPrior knowledge required: " + priorKnowledgeRequired + "\n");
+		String label;
+		if (totalPrice < 500) {
+			label = "CHEAP";
+		} else if (totalPrice >= 500 && totalPrice <= 1500) {
+			label = "OK";
+		} else {
+			label = "EXPENSIVE";
+		}
+
+		bw.write("That price is " + label + "\n");
+		bw.write("There are " + instructors.size() + " instructors for this course:" + "\n");
+		for (String instructor : instructors) {
+			bw.write(instructor + "\n");
+		}
+
+		bw.close();
+		System.out.println("info written to courseinfo"+title+".txt");
+	}
+	
+	
+
 }
